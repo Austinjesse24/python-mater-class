519b1211e124ca2624272b5952c79ecce30a59c7
Austin Jesse Agaba <austinjesse256@gmail.com>
2025-01-18 12:12:45 +0300

    addition on positive-negative.
    condition statements

--- positive-negative.py
+++ positive-negative.py
@@ -1,7 +1,18 @@
-num = 10
-if num > 0:
-    print("positive number")
-elif num == 0:
-    print("zero")
+# num = 10
+# if num > 0:
+#     print("positive number")
+# elif num == 0:
+#     print("zero")
+# else:
+#     print("negative number")
+
+code = 50
+
+if code >= 90:
+    print("grade A")
+elif code >= 80:
+    print("grade B")
+elif code >= 50:
+    print("grade C")
 else:
-    print("negative number")
+    print("Failed")
