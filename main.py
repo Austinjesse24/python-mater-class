563910d4d320d6a232be939f2686202ca333f63f
Austin Jesse Agaba <austinjesse256@gmail.com>
2025-01-18 11:11:59 +0300

    Create main.py
    this describes data types in python

---
+++ main.py
@@ -0,0 +1,8 @@
+int = 21
+str = "hello world"
+float = 3.14
+tuple = (1, 2, 3, 4, 5)
+list = [1, 2, 3, 4, 5]
+set = {1, 2, 3, 4, 5}
+dict = {1:"one", 2:"two", 3:"three", 4:"four", 5:"five"}
+print(int)
