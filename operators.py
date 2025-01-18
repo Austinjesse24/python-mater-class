567be8acb2ffd743cd38f0757015a1ffc04aa062
Austin Jesse Agaba <austinjesse256@gmail.com>
2025-01-18 11:35:02 +0300

    operations in py
    these the operations in python i am now conversant with operations in python

--- operators.py
+++ operators.py
@@ -4,4 +4,22 @@ y = 3.12
 
 answer = x + y
 
-print(round(answer, 2))
+# print(round(answer, 2))
+
+# multiplication in python
+a = 21
+b = 45
+# print(a * b)
+
+# subtraction in python
+c = 50
+d = 24
+# print(a - b)
+
+# division in python
+
+e = 10
+d = 3
+
+ed = e / d
+print(round(ed, 2))
