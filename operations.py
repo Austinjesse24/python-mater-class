900c687719a79bae6d37dbd05efdf3cb90231800
Austin Jesse Agaba <austinjesse256@gmail.com>
2025-01-18 11:55:06 +0300

    basic temp coverter
    basic function on how to convert fron celsius to fahrenheit

---
+++ operations.py
@@ -0,0 +1,9 @@
+# celsius = float(input("32"))
+
+celsius = 32
+
+fahrenheit = (celsius * 9/5) + 32
+
+# print(fahrenheit)
+
+print(str (celsius) + "degrees celsius is equal to" + str (fahrenheit) + "degrees fahrenheit")
