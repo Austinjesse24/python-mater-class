787fa38632474fa30ebcd129f59a9c0a33847990
Austin Jesse Agaba <austinjesse256@gmail.com>
2025-01-20 16:22:36 +0300

    sets in python
    this shows how to get ,add , and display content in a set

---
+++ sets.py
@@ -0,0 +1,17 @@
+# create empty set
+
+s = set()
+
+# add elements to the set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+# print(s)
+
+# no elenments are repeated in a set
+
+# to remove elements from a set
+s.remove(2) 
+
+print(f"The set has {len(s)} elements.")
