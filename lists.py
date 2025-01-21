fa82aeaf1b57b386f4b88813b47b040cede7a3c2
Austin Jesse Agaba <austinjesse256@gmail.com>
2025-01-21 23:30:22 +0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,6 +1,6 @@
 # define a listof names   
 name =["Harry","Ron","Hermione","Ginny"]
 # (name[0])
-# name.append("Draco")
-# name.sort()
-# print(name)
+name.append("Draco")
+name.sort()
+print(name)
