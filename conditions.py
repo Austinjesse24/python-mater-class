a27f5e3a33ae604f718a6947c58b377233c73a6c
Austin Jesse Agaba <austinjesse256@gmail.com>
2025-01-21 23:47:46 +0300

    Update conditions.py

--- conditions.py
+++ conditions.py
@@ -1,8 +1,8 @@
 n = int(input("Number: "))
 
-# if n > 0:
-#     print("n is positive")
-# elif n < 0:
-#     print("n is negative")
-# else:
-#     print("n is zero")
+if n > 0:
+    print("n is positive")
+elif n < 0:
+    print("n is negative")
+else:
+    print("n is zero")
